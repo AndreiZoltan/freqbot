46b169556ed8c0d1055538a235ca64ae1d406c02
AndrewSultan <andrew15sultan@gmail.com>
2020-08-10 11:59:17 +0300

    seems that trade is done

--- freqbot/algorithms/basic_algorithm.py
+++ freqbot/algorithms/basic_algorithm.py
@@ -2,9 +2,11 @@ import pandas as pd
 
 
 class BasicAlgorithm:
-    def __init__(self, tick_type: str, size):
+    def __init__(self, tick_type: str, tick_size, order_type: str = 'MARKET'):
         self.tick_type = tick_type
-        self.size = size
+        self.tick_size = tick_size
+        self.order_type = order_type
+        self.price = 0
         self.state = pd.DataFrame()
 
     def get_state(self, state):
--- freqbot/bot.py
+++ freqbot/bot.py
@@ -12,7 +12,9 @@ class Bot:
         self.bm = BinanceSocketManager(self.client)
         self.algorithm = algorithm
         self.data = pd.DataFrame()
+        self.stake_amount = 0
         self.request = dict()
+        self.order = dict()
 
     def process_message(self, message):
         message["id"] = message.pop("a")
@@ -39,7 +41,7 @@ class Bot:
             print(self.data.iloc[0, 0], self.data.iloc[-1, 0])
         print(self.data.shape)
         self.data = self.data.append(message, ignore_index=True)
-        state = getattr(self.data.bars, self.algorithm.tick_type)(self.algorithm.size)
+        state = getattr(self.data.bars, self.algorithm.tick_type)(self.algorithm.tick_size)
         if not state.empty:
             self.algorithm.get_state(state)
             action = self.algorithm.action()
@@ -48,7 +50,7 @@ class Bot:
 
     def get_initial_data(self, pair: str, days: int, override: bool):
         path = os.path.abspath(__file__)
-        path = "/".join(path.split("/")[:-1]) + '/data/'
+        path = "/".join(path.split('/')[:-1]) + '/data/'
         print(pair, days, path)
         self.data = fm.load_dataset(client=self.client,
                                     pair=pair,
@@ -57,7 +59,7 @@ class Bot:
                                     override=override,
                                     use_swifter=False)
 
-        state = getattr(self.data.bars, self.algorithm.tick_type)(self.algorithm.size)
+        state = getattr(self.data.bars, self.algorithm.tick_type)(self.algorithm.tick_size)
 
         self.algorithm.get_state(state)
 
@@ -77,17 +79,38 @@ class Bot:
         message = self.process_message(message)
         self.update(message)
 
-    def act(self, action):
-        pass
+    def create_request(self, pair: str):
+        self.request['symbol'] = pair
+        self.request['type'] = self.algorithm.order_type
+        self.request['newOrderRespType'] = 'FULL'
+        self.request['recvWindow'] = 250  # why not?
+        if self.algorithm.order_type == 'MARKET':
+            self.request['quoteOrderQty'] = self.stake_amount
+        else:  # for LIMIT type
+            self.request['timeInForce'] = 'GTC'
 
-    def trade(self, pair: str, days: int, override: bool = True):
+    def make_request(self, action):
+        self.request['side'] = action
+        if self.algorithm.order_type == 'LIMIT':
+            self.request['price'] = str(self.algorithm.price)
+            self.request['quantity'] = self.algorithm.price * self.stake_amount
+
+    def act(self, action):
+        if action:
+            self.make_request(action)
+            self.order = self.client.create_order(** self.request)
+
+    def trade(self, pair: str, days: int, override: bool = True, stake_amount: int = 10):
         """
         This function is used for trading
         :param pair: pair to trade on
         :param days: number of days from which data is collected
         :param override: should data be override ?
+        :param stake_amount: amount of one stake
         :return: trade function has no return but it saves logs
         """
+        self.stake_amount = stake_amount
+        self.create_request(pair)
         self.get_initial_data(pair=pair, days=days, override=override)
         conn_key = self.bm.start_aggtrade_socket(pair, self.handle_message)
         self.bm.start()
