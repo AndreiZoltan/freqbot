3b88be03145c03f163ff0be302abed4165d8a3cb
AndrewSultan <andrew15sultan@gmail.com>
2020-08-11 17:30:29 +0300

    resolve quantity bug

--- freqbot/bot.py
+++ freqbot/bot.py
@@ -12,9 +12,15 @@ class Bot:
         self.bm = BinanceSocketManager(self.client)
         self.algorithm = algorithm
         self.data = pd.DataFrame()
-        self.stake_amount = 0
         self.request = dict()
         self.order = None
+
+        # some metadata
+        self.stake_amount = 0
+        self.quantity = 0
+        self.price = 0
+        self.lot_precision = 0
+        self.price_precision = 0
 
     @property
     def is_trading(self):
@@ -23,9 +29,27 @@ class Bot:
             return False if self.order['status'] == 'FILLED' else True
         return False
 
+    def set_metadata(self, pair, stake_amount):
+        def get_precision(string: str):
+            precision = 0
+            for char in string:
+                if char == '1':
+                    break
+                precision += 1 if char == '0' else 0
+            return precision
+
+        self.stake_amount = stake_amount
+        info = self.client.get_symbol_info(pair)
+        step_size = info['filters'][2]['stepSize']
+        self.lot_precision = get_precision(step_size)
+        tick_size = info['filters'][0]['tickSize']
+        self.price_precision = get_precision(tick_size)
+        self.make_request = self.make_limit_request if self.algorithm.order_type == 'LIMIT' else self.make_market_request
+
     def process_message(self, message):
         message["id"] = message.pop("a")
         message["price"] = message.pop("p")
+        self.price = float(message["price"])
         message["amount"] = message.pop("q")
         message["timestamp"] = message.pop("T")
         message["datetime"] = pd.to_datetime(message["timestamp"],
@@ -91,16 +115,24 @@ class Bot:
         self.request['type'] = self.algorithm.order_type
         self.request['newOrderRespType'] = 'FULL'
         self.request['recvWindow'] = 250  # why not?
-        if self.algorithm.order_type == 'MARKET':
-            self.request['quoteOrderQty'] = self.stake_amount
-        else:  # for LIMIT type
+        if self.algorithm.order_type == 'LIMIT':  # for LIMIT type
             self.request['timeInForce'] = 'GTC'
 
-    def make_request(self, action):
+    def make_limit_request(self, action: str):
         self.request['side'] = action
-        if self.algorithm.order_type == 'LIMIT':
-            self.request['price'] = str(self.algorithm.price)
-            self.request['quantity'] = self.stake_amount / self.algorithm.price
+
+        # may be better to use self.price + n * tick_size instead of self.algorithm.price
+        # or IOC with self.price - n * tick_size
+        self.request['price'] = "{:0.0{}f}".format(self.algorithm.price, self.price_precision)
+        if action == 'BUY':
+            self.quantity = "{:0.0{}f}".format(self.stake_amount / self.algorithm.price, self.lot_precision)
+            self.request['quantity'] = self.quantity
+
+    def make_market_request(self, action: str):
+        self.request['side'] = action
+        if action == 'BUY':
+            self.quantity = "{:0.0{}f}".format(self.stake_amount / self.price, self.lot_precision)
+            self.request['quantity'] = self.quantity
 
     def act(self, action):
         if action:
@@ -117,7 +149,7 @@ class Bot:
         :param stake_amount: amount of one stake
         :return: trade function has no return but it saves logs
         """
-        self.stake_amount = stake_amount
+        self.set_metadata(pair, stake_amount)
         self.create_request(pair)
         self.get_initial_data(pair=pair, days=days, override=override)
         conn_key = self.bm.start_aggtrade_socket(pair, self.handle_message)
